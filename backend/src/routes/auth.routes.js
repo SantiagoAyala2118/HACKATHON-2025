df890622114b834c68da13b04109ebd9e4caf2fe
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-16 17:22:56 -0300

    Validator creado, validationes de auth creadas y añadidas a las rutas correspondientes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { login, logout, register } from "../controllers/user.controllers.js";
+//* CONTROLADORES
+import { login, logout, register } from "../controllers/auth.controllers.js";
+
+//* MIDDLEWARES
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import {
+  registerValidations,
+  loginValidations,
+} from "../middlewares/validations/auth.validations.js";
 
 const authRouter = Router();
 
-authRouter.post("/register", register);
-authRouter.post("/login", login);
+authRouter.post("/register", registerValidations, register);
+authRouter.post("/login", loginValidations, login);
 authRouter.post("/logout", authMiddleware, logout);
 
 export default authRouter;
