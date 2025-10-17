0fe44cca64beee6b58cd5d6981bd4443763e0b80
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-16 22:30:35 -0300

    Se añadieron los middlewares de validaciones a las rutas

--- backend/src/routes/investment.routes.js
+++ backend/src/routes/investment.routes.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { addInvestment } from "../controllers/investment.controller.js";
+import { createInvestmentValidations } from "../middlewares/validations/investment.validations.js";
+import { validator } from "../middlewares/validator.js";
 
 export const investmentRouter = Router();
 
-investmentRouter.post("/investment", authMiddleware, addInvestment);
+investmentRouter.post(
+  "/investment",
+  authMiddleware,
+  createInvestmentValidations,
+  validator,
+  addInvestment
+);
